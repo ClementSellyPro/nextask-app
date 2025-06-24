7315471d76d2f810a13ecd190a74e275c696b7f9
ClementSellyPro <clementselly.pro@gmail.com>
2025-06-24 15:49:19 +0400

    Create TaskColumnComponent and ProjectBoardComponent to handle the columns - Add style to these components

---
+++ src/app/components/project-board/project-board.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { TaskColumnComponent } from '../task-column/task-column.component';
+
+@Component({
+  selector: 'app-project-board',
+  imports: [
+    TaskColumnComponent
+  ],
+  templateUrl: './project-board.component.html',
+  styleUrl: './project-board.component.css'
+})
+export class ProjectBoardComponent {
+
+}
--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -1,15 +1,16 @@
 import { Component } from '@angular/core';
 import { DashboardHeaderComponent } from '../../components/dashboard-header/dashboard-header.component';
+import { ProjectBoardComponent } from "../../components/project-board/project-board.component";
 
 @Component({
   selector: 'app-dashboard',
   imports: [
     DashboardHeaderComponent,
-  ],
+    ProjectBoardComponent
+],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.css'
 })
 export class DashboardComponent {
-  isModalOpen: boolean = false;
 
 }
