b1bf8b18e644acf5022533af2faa287d6e5108ff
ClementSellyPro <clementselly.pro@gmail.com>
2025-06-25 15:37:36 +0400

    Change HeaderComponent according the URL - Display buttons for URL '/', hide them for '/auth', display user-icon for '/dashboard' + box-shadow

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { NgClass, NgIf } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { NavigationEnd, Router, RouterLink } from '@angular/router';
+import { filter } from 'rxjs';
 
 @Component({
   selector: 'app-header',
-  imports: [RouterLink],
+  imports: [RouterLink, NgClass],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css',
 })
-export class HeaderComponent {}
+export class HeaderComponent implements OnInit {
+  currentUrl: string = '';
+
+  constructor(private router: Router) {}
+
+  ngOnInit(): void {
+    this.router.events
+      .pipe(filter((event) => event instanceof NavigationEnd))
+      .subscribe((event: NavigationEnd) => {
+        this.currentUrl = event.url;
+      });
+
+    this.currentUrl = this.router.url;
+  }
+}
