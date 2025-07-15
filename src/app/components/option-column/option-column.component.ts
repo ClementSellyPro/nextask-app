1ab60bd557fee7825e95e90b39e94e5633584a84
ClementSellyPro <clementselly.pro@gmail.com>
2025-07-15 10:34:37 +0400

    Add a form to update TaskColumn info (title and color) - Bind the form with the current info

--- src/app/components/option-column/option-column.component.ts
+++ src/app/components/option-column/option-column.component.ts
@@ -1,16 +1,28 @@
 import { NgIf } from '@angular/common';
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { ClickOutsideDirective } from '../../directives/click-outside.directive';
+import { FormsModule } from '@angular/forms';
+import { ColorPicker } from 'primeng/colorpicker';
 
 @Component({
   selector: 'app-option-column',
-  imports: [NgIf, ClickOutsideDirective],
+  imports: [NgIf, ClickOutsideDirective, FormsModule, ColorPicker],
   templateUrl: './option-column.component.html',
   styleUrl: './option-column.component.css',
 })
-export class OptionColumnComponent {
+export class OptionColumnComponent implements OnInit {
   @Output() deleteColumnEvent = new EventEmitter<void>();
   isMenuOpen: boolean = false;
+  isUpdatingColumn: boolean = false;
+  @Input() currentTitle!: string;
+  @Input() currentColor!: string;
+  titleUpdate!: string;
+  colorUpdate!: string;
+
+  ngOnInit(): void {
+    this.titleUpdate = this.currentTitle;
+    this.colorUpdate = this.currentColor;
+  }
 
   toggleMenu() {
     this.isMenuOpen = !this.isMenuOpen;
@@ -22,5 +34,17 @@ export class OptionColumnComponent {
 
   onDeleteColumn() {
     this.deleteColumnEvent.emit();
+  }
+
+  onOpenUpdateForm() {
+    this.isUpdatingColumn = true;
+    this.isMenuOpen = false;
+  }
+
+  onSaveUpdateColumn() {}
+
+  onCancelUpdateColumn() {
+    this.isUpdatingColumn = false;
+    this.isMenuOpen = true;
   }
 }
