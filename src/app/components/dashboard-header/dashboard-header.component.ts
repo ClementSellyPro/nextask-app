22529bf92ac612edaf92d8d188998fe1d22d007f
ClementSellyPro <clementselly.pro@gmail.com>
2025-08-03 19:15:07 +0400

    Get properly the Project title from ProjectService

--- src/app/components/dashboard-header/dashboard-header.component.ts
+++ src/app/components/dashboard-header/dashboard-header.component.ts
@@ -1,16 +1,41 @@
-import { NgIf } from '@angular/common';
-import { Component } from '@angular/core';
+import { AsyncPipe, NgIf } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
 import { ModalFilterComponent } from '../modal-filter/modal-filter.component';
+import { ProjectService } from '../../services/project.service';
+import { BehaviorSubject, Observable } from 'rxjs';
+import { Project } from '../../models/Project.model';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-dashboard-header',
-  imports: [ModalFilterComponent, NgIf],
+  imports: [ModalFilterComponent, NgIf, AsyncPipe, FormsModule],
   templateUrl: './dashboard-header.component.html',
   styleUrl: './dashboard-header.component.css',
 })
-export class DashboardHeaderComponent {
+export class DashboardHeaderComponent implements OnInit {
+  defaultProject: Project = {
+    id: '',
+    name: '',
+    columns: [],
+  };
+
   isModalOpen: boolean = false;
   isUpdatingTitle: boolean = false;
+  project: BehaviorSubject<Project> = new BehaviorSubject(this.defaultProject);
+  project$: Observable<Project> = this.project.asObservable();
+  titleUpdate!: string;
+
+  constructor(private projectService: ProjectService) {}
+
+  ngOnInit(): void {
+    this.loadProject();
+  }
+
+  loadProject() {
+    this.projectService.getCurrentProject().subscribe((data) => {
+      this.project.next(data);
+    });
+  }
 
   onFilterClick() {
     this.isModalOpen = !this.isModalOpen;
@@ -25,6 +50,9 @@ export class DashboardHeaderComponent {
   }
 
   saveTitleInput() {
-    this.isUpdatingTitle = false;
+    if (this.titleUpdate) {
+      this.projectService.updateProjectName(this.titleUpdate);
+      this.isUpdatingTitle = false;
+    }
   }
 }
